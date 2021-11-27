59ed2323666a812420772b9679dfa2524ac507f5
Andrii Martseniuk <andry.martsenyk@gmail.com>
2021-11-28 00:43:41 +0200

    design 4 chat

--- app/build/generated/data_binding_base_class_source_out/debug/out/de/event/databinding/ActivityChatBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/de/event/databinding/ActivityChatBinding.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -31,14 +32,45 @@ public final class ActivityChatBinding implements ViewBinding {
   public final EditText edMessage;
 
   @NonNull
+  public final ImageView imageView;
+
+  @NonNull
+  public final ImageView imageView2;
+
+  @NonNull
+  public final ImageView imageView3;
+
+  @NonNull
+  public final ImageView imageView4;
+
+  @NonNull
+  public final ImageView imageView5;
+
+  @NonNull
+  public final ImageView imageView6;
+
+  @NonNull
+  public final ImageView imageView7;
+
+  @NonNull
   public final RecyclerView rcView;
 
   private ActivityChatBinding(@NonNull ConstraintLayout rootView, @NonNull Button bSend,
-      @NonNull ConstraintLayout bsend, @NonNull EditText edMessage, @NonNull RecyclerView rcView) {
+      @NonNull ConstraintLayout bsend, @NonNull EditText edMessage, @NonNull ImageView imageView,
+      @NonNull ImageView imageView2, @NonNull ImageView imageView3, @NonNull ImageView imageView4,
+      @NonNull ImageView imageView5, @NonNull ImageView imageView6, @NonNull ImageView imageView7,
+      @NonNull RecyclerView rcView) {
     this.rootView = rootView;
     this.bSend = bSend;
     this.bsend = bsend;
     this.edMessage = edMessage;
+    this.imageView = imageView;
+    this.imageView2 = imageView2;
+    this.imageView3 = imageView3;
+    this.imageView4 = imageView4;
+    this.imageView5 = imageView5;
+    this.imageView6 = imageView6;
+    this.imageView7 = imageView7;
     this.rcView = rcView;
   }
 
@@ -83,13 +115,57 @@ public final class ActivityChatBinding implements ViewBinding {
         break missingId;
       }
 
+      id = R.id.imageView;
+      ImageView imageView = ViewBindings.findChildViewById(rootView, id);
+      if (imageView == null) {
+        break missingId;
+      }
+
+      id = R.id.imageView2;
+      ImageView imageView2 = ViewBindings.findChildViewById(rootView, id);
+      if (imageView2 == null) {
+        break missingId;
+      }
+
+      id = R.id.imageView3;
+      ImageView imageView3 = ViewBindings.findChildViewById(rootView, id);
+      if (imageView3 == null) {
+        break missingId;
+      }
+
+      id = R.id.imageView4;
+      ImageView imageView4 = ViewBindings.findChildViewById(rootView, id);
+      if (imageView4 == null) {
+        break missingId;
+      }
+
+      id = R.id.imageView5;
+      ImageView imageView5 = ViewBindings.findChildViewById(rootView, id);
+      if (imageView5 == null) {
+        break missingId;
+      }
+
+      id = R.id.imageView6;
+      ImageView imageView6 = ViewBindings.findChildViewById(rootView, id);
+      if (imageView6 == null) {
+        break missingId;
+      }
+
+      id = R.id.imageView7;
+      ImageView imageView7 = ViewBindings.findChildViewById(rootView, id);
+      if (imageView7 == null) {
+        break missingId;
+      }
+
       id = R.id.rcView;
       RecyclerView rcView = ViewBindings.findChildViewById(rootView, id);
       if (rcView == null) {
         break missingId;
       }
 
-      return new ActivityChatBinding((ConstraintLayout) rootView, bSend, bsend, edMessage, rcView);
+      return new ActivityChatBinding((ConstraintLayout) rootView, bSend, bsend, edMessage,
+          imageView, imageView2, imageView3, imageView4, imageView5, imageView6, imageView7,
+          rcView);
     }
     String missingId = rootView.getResources().getResourceName(id);
     throw new NullPointerException("Missing required view with ID: ".concat(missingId));
